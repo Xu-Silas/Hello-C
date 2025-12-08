aa341151f9ddf5a4ecb2af9c09472c5e7125bd06
Xu-Silas <13340206629@163.com>
2025-12-08 13:55:10 +0800

    Create HelloWorld.cpp

---
+++ HelloWorld.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+	string name;
+	cout<<"please enter your name"<<endl;
+	cin>>name;
+	cout<<"Hello,C++!"<<endl;
+	cout<<"My name is "<<name<<"!";
+	return 0;
+}
