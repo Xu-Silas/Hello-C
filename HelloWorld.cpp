eedeeb079b72d804fb2ed9a1bf745d35e1331438
Xu-Silas <13340206629@163.com>
2025-12-09 11:35:24 +0800

    Update HelloWorld.cpp

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -7,7 +7,7 @@ int main()
 	string name;
 	cout<<"please enter your name"<<endl;
 	cin>>name;
-	cout<<"Hello,World"<<endl;
+	cout<<"Hello,C++"<<endl;
 	cout<<"My name is "<<name<<"!";
 	return 0;
 }
